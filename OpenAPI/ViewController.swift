95e09ccda7a65ddedf282e552371e246fe838fdc
f.shirakura <apple@f-shirakura.local>
2016-08-21 01:46:07 +0900

    first

--- OpenAPI/ViewController.swift
+++ OpenAPI/ViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let hostName = "www.chiikinogennki.soumu.go.jp"
+        let version = "v001"
+        let jenre = "博物館"
+        let outputFormat = "json"
+        let parameter = "limit=10"
+        
+        var urlString = "https://\(hostName)/k-cloud-api/\(version)/kanko/\(jenre)/\(outputFormat)?\(parameter)"
+        
+        urlString = urlString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!
+        
+
+        
+        Alamofire.request(.GET, urlString)
+        .responseJSON { response in
+            
+            
+            let resultValue = response.result.value!
+            let jsonData = resultValue["tourspots"] as AnyObject!
+            let json = JSON(jsonData)
+            
+            
+            json.forEach({ (_, tempjson) in
+                let name = tempjson["name"]["name1"]["written"].string
+                print(name)
+            })
+            
+            
+     
+        }
+        
+        // https://<ホスト名>/k-cloud-api/<バージョン>/kanko/<ジャンル>/<出力データ形式>?<パラメータ群>
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+use_frameworks!
+source 'https://github.com/CocoaPods/Specs.git'
+target "OpenAPI" do
+    pod 'Alamofire'
+   pod 'SwiftyJSON'
+end
